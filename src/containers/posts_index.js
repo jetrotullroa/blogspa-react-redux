285f7b91fefeb23d7d0146ef86b8c4efa9a8b415
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-21 10:33:56 +0800

    create container dir to handle smart component. create first smart component that will handle all blog post

---
+++ src/containers/posts_index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class PostsIndex extends React.Component {
+  render() {
+    return(
+      <div>
+        <h4>Recent Post</h4>
+      </div>
+    )
+  }
+}
+
+export default PostsIndex
