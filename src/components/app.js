b8cc9dde91d7461683dc757baff8f81c701f7c8c
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-21 11:21:11 +0800

    create nav header

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './header'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>blog Single Page Application</h1>
-        <div className="content">
+        <Header />
+        <div className="content container">
           {this.props.children}
         </div>
       </div>
---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = () => {
+  return(
+    <nav>
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo">blogSPA</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
