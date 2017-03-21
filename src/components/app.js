b15410bc035e982fb93f4b98939c8eb3ad744d54
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-21 10:33:09 +0800

    change app to be the parent component and will render child component/container via react-router

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h1>blog Single Page Application</h1>
+        <div className="content">
+          {this.props.children}
+        </div>
+      </div>
     );
   }
 }
