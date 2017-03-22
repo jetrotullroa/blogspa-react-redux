cce751cd2e6a85dd779c932631dbe3f879eb0c50
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-22 16:08:15 +0800

    render posts

--- src/containers/posts_index.js
+++ src/containers/posts_index.js
@@ -1,14 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchPosts } from '../actions'
 
 
 class PostsIndex extends React.Component {
+
+  componentWillMount() {
+    this.props.fetchPosts()
+  }
+
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return(
+        <li className="collection-item" key={post.id}>
+          <strong>{post.title}</strong>
+          <span className="secondary-content">{post.categories}</span>
+        </li>
+      )
+    })
+  }
+
   render() {
     return(
       <div>
-        <h4>Recent Post</h4>
+        <ul className="collection with-header">
+          <li className="collection-header"><h4>Recent Posts</h4></li>
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
 
-export default PostsIndex
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ fetchPosts: fetchPosts }, dispatch)
+}
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts.all
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex)
