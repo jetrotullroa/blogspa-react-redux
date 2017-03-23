81bd67705a8724c9748b239fd9c07f859d283b98
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-23 10:00:25 +0800

    create container tha will handle form for new posts

---
+++ src/containers/posts_new.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class PostsNew extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2>New Posts</h2>
+        <form action="">
+          
+        </form>
+      </div>
+    )
+  }
+}
+
+export default PostsNew
