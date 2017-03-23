91ffe81ec427a58e8c66d41e6a3436cb31b79ef2
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-23 11:19:05 +0800

    create basic form

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Row, Input, Button } from 'react-materialize'
 
 class PostsNew extends React.Component {
   render() {
@@ -6,7 +7,12 @@ class PostsNew extends React.Component {
       <div>
         <h2>New Posts</h2>
         <form action="">
-          
+          <Row>
+            <Input s={12} label="Title" />
+            <Input s={12} label="Categories" />
+            <Input s={12} label="Content" type="textarea" />
+            <Button waves="light">Submit</Button>
+          </Row>
         </form>
       </div>
     )
