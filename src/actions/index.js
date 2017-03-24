b683de3863c102281e08d0b76de2d02d6ee9c5b4
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-24 18:25:47 +0800

    create action for creating post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const FETCH_POSTS = 'FETCH_POSTS'
+export const CREATE_POST = 'CREATE_POST'
 
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
@@ -11,6 +12,14 @@ export const fetchPosts = () => {
   const request = axios.get(`${ROOT_URL}/posts?key=${API_KEY}`)
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export const createPost = () => {
+  const request = axios.post(`${ROOT_URL}/posts?key=${API_KEY}`)
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
