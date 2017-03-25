6c29024dec22a9bcf2bdae84fbdf6c141c0d2bf2
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-25 23:15:25 +0800

    add validation

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -7,6 +7,9 @@ class PostsNew extends React.Component {
 
   onSubmitForm(props) {
     this.props.createPost(props)
+      .then(() => {
+        browserHistory.push('/')
+      })
   }
 
   render() {
@@ -16,20 +19,29 @@ class PostsNew extends React.Component {
       <div>
         <h3>New Post</h3>
 
-        <form onSubmit={handleSubmit(this.onSubmitForm.bind(this))}>
-          <div className="form-group">
+        <form  className="post-form" onSubmit={handleSubmit(this.onSubmitForm.bind(this))}>
+          <div className={`input-field ${title.touched && title.invalid ? 'has-danger' : ''}`}>
             <label htmlFor="title">Title</label>
-            <input type="text" className="form-control" {...title} />
+            <input type="text" className="form" {...title} />
+            <div className="text-help">
+              {title.touched ? title.error : ''}
+            </div>
           </div>
 
-          <div className="form-group">
+          <div className={`input-field ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
             <label htmlFor="categories">Category</label>
-            <input type="text" className="form-control" {...categories} />
+            <input type="text" className="form" {...categories} />
+            <div className="text-help">
+              {categories.touched ? categories.error : ''}
+            </div>
           </div>
 
-          <div className="form-group">
+          <div className={`input-field ${content.touched && content.invalid ? 'has-danger' : ''}`}>
             <label htmlFor="content">Content</label>
-            <textarea type="text" className="form-control" {...content} />
+            <textarea type="text" className="form" {...content} />
+            <div className="text-help">
+              {content.touched ? content.error : ''}
+            </div>
           </div>
 
           <div className="form-group">
@@ -44,7 +56,25 @@ class PostsNew extends React.Component {
   }
 }
 
+const validate = (values) => {
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = "There is no title"
+  }
+
+  if (!values.categories) {
+    errors.categories = "There is no category"
+  }
+
+  if (!values.content) {
+    errors.content = "There is no content"
+  }
+
+  return errors
+}
+
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'], validate
 }, null, { createPost })(PostsNew)
